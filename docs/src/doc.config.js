dd116bf4066f9357bacb05245ebcef186d4bf958
febobo <584516601@qq.com>
2017-12-20 17:41:51 +0800

    update doc

--- docs/src/doc.config.js
+++ docs/src/doc.config.js
@@ -17,92 +17,90 @@ module.exports = {
     },
     logo: 'https://vue-js.com/public/images/vue.png',
     title: 'MILK',
-    nav: [
-      {
+    nav: [{
         'name': '开发指南',
-        'groups': [
-          {
-            'list': [
-              {
-                'path': '/quickstart',
-                'title': '快速上手',
-                noExample: true
-              },
-              {
-                'title': '业务组件',
-                'link': '/zanui/captain/component/quickstart'
-              },
-              {
-                'path': '/changelog',
-                'title': '更新日志',
-                noExample: true
-              }
-            ]
-          }
-        ]
+        'groups': [{
+          'list': [{
+              'path': '/quickstart',
+              'title': '快速上手',
+              noExample: true
+            },
+            // {
+            //   'title': '业务组件',
+            //   'link': '/zanui/captain/component/quickstart'
+            // },
+            {
+              'path': '/changelog',
+              'title': '更新日志',
+              noExample: true
+            }
+          ]
+        }]
       },
       {
         'name': '组件',
         'showInMobile': true,
-        'groups': [
-          {
-            'groupName': '基础组件',
-            'list': [
-              {
-                'path': '/layout',
-                'title': 'Layout - 布局'
-              },
-              {
-                'path': '/icon',
-                'title': 'Icon - 图标'
-              },
-              {
-                'path': '/button',
-                'title': 'Button - 按钮'
-              },
-              {
-                'path': '/input',
-                'title': 'Input - 输入框'
-              },
-              {
-                'path': '/toast',
-                'title': 'Toast - 轻提示'
-              },
-              {
-                'path': '/textarea',
-                'title': 'Textarea - 多行输入'
-              },
-              {
-                'path': '/list',
-                'title': 'List - 列表'
-              },
-              {
-                'path': '/navBar',
-                'title': 'NavBar - 导航栏'
-              },
-              {
-                'path': '/drawer',
-                'title': 'Drawer - 抽屉'
-              },
-              {
-                'path': '/menu',
-                'title': 'Menu - 菜单'
-              },
-              {
-                'path': '/radio',
-                'title': 'Radio - 单选框'
-              },
-              {
-                'path': '/checkbox',
-                'title': 'Checkbox - 复选框'
-              },
-              {
-                'path': '/switch',
-                'title': 'Switch - 滑动开关'
-              }
-            ]
-          }
-        ]
+        'groups': [{
+          'groupName': '基础组件',
+          'list': [{
+              'path': '/layout',
+              'title': 'Layout - 布局'
+            },
+            {
+              'path': '/icon',
+              'title': 'Icon - 图标'
+            },
+            {
+              'path': '/button',
+              'title': 'Button - 按钮'
+            },
+            {
+              'path': '/list',
+              'title': 'List - 列表'
+            },
+            {
+              'path': '/navBar',
+              'title': 'NavBar - 导航栏'
+            },
+            {
+              'path': '/drawer',
+              'title': 'Drawer - 抽屉'
+            },
+            {
+              'path': '/menu',
+              'title': 'Menu - 菜单'
+            },
+            {
+              'path': '/switch',
+              'title': 'Switch - 滑动开关'
+            }
+          ]
+        }, {
+          'groupName': '表单',
+          'list': [{
+              'path': '/input',
+              'title': 'Input - 输入框'
+            },
+            {
+              'path': '/textarea',
+              'title': 'Textarea - 多行输入'
+            },
+            {
+              'path': '/radio',
+              'title': 'Radio - 单选框'
+            },
+            {
+              'path': '/checkbox',
+              'title': 'Checkbox - 复选框'
+            },
+          ]
+        }, {
+          'groupName': '操作反溃',
+          'list': [{
+            'path': '/toast',
+            'title': 'Toast - 轻提示'
+          }, ]
+        }]
       }
     ]
   }
